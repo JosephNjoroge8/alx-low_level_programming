7fceaf271c408fdae8b4a8160f662edeafcd84a7
Njoro444code <joem7mach@gmail.com>
2023-03-22 18:19:50 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_abs - checks our code
+ *@h: integer to calculate the absolute value
+ *Return: always 0
+ */
+int _abs(int h)
+{
+	if (h < 0)
+	{
+		return (-h);
+	}
+	return (h);
+}
